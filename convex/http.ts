1ac782f3d75f31a13317c69ea55dead2dda0c22a
muhammadfarhan19 <hamidie.farhan@gmail.com>
2024-10-08 09:35:15 +0700

    fix

--- convex/http.ts
+++ convex/http.ts
@@ -1,7 +1,7 @@
 import { httpRouter } from "convex/server";
 import { httpAction } from "./_generated/server";
 import { internal } from "./_generated/api";
-import { WebhookEvent } from "@clerk/nextjs/server";
+import { WebhookEvent } from "@clerk/backend";
 import { Webhook } from "svix";
 
 const http = httpRouter();
